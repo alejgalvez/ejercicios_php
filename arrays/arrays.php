97deccb3ae2f9ad5c275d88ce8f132bd99bd5ce8
alejgalvez <alejglvezg@gmail.com>
2025-11-06 12:00:10 +0100

    prueba_php

---
+++ arrays/arrays.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+    /*
+
+    // Definir un array con los modulos del curso 2º DAW
+     $modulos = ["Entorno Servidor", "Entorno cliente", "Ingles"];
+
+     // Modificar el valor del segundo modulo
+     $modulos[2] = "Aplicaciones web";
+
+     // Añadir un nuevo modulo al final del array
+     $modulos[] = "Ingles";
+
+     // Imprime los valores del array
+     print_r("<h1>Este es el segundo modulo del curso 2º DAW: $modulos[2] </h1>");
+
+
+     $usuario = [
+         'nombre' => 'Juan',
+         'email' => 'juan@gmail.com',
+         'edad' => 21
+     ];
+     // Imprime el email del usuario
+     print_r("<h2>El nombre del usuario es:" . $usuario['email'] . "</h2>");
+
+     // Cambia la edad del usuario
+     $usuario['edad'] = 32;
+
+     // Imprime la edad del usuario ya cambiada
+     print_r("<h2>La edad del usuario es: " . $usuario['edad'] . "</h2>");
+
+     //Suma de dos numeros usando un array
+     $suma = [5, 2];
+     $resultado = $suma[0] + $suma[1];
+     print_r("<h3>El resultado de la suma es: $resultado </h3>");
+
+     $suma = [
+         'a' => 5,
+         'b' => 2
+     ];
+     $resultado = $suma['a'] + $suma['b'];
+     print_r("<h3>El resultado de la suma es: $resultado </h3>");
+
+     $frutas = ["manzana", "pera", "naranja", "plátano"];
+     echo "";
+     foreach ($frutas as $fruta) {
+         echo "<p>$fruta</p>";
+     }
+     echo "";
+
+
+    $usuario = ["nombre" => "Juan", "edad" => 25];
+    echo "";
+    foreach ($usuario as $clave => $valor) {
+        echo "<p>$clave: $valor</p>";
+    }
+    echo "";
+
+    */
+
+    ?>
+
+
+</body>
+
+</html>
